d1217da23630e3ddb3572a947a5f52f279dbad41
eamonmoradi <72042248+eamonmoradi@users.noreply.github.com>
2021-05-12 11:09:18 -0700

    Added getUser setUser and chargerHistory

--- functions/routes/chargers.js
+++ functions/routes/chargers.js
@@ -6,9 +6,13 @@ router.get('/', checkIfAuthenticated, controller.getAllChargers);
 router.get('/getCurrent/:chargerId', controller.getCurrent);
 router.get('/getTemperature/:chargerId', controller.getTemperature);
 router.get('/getPaymentState/:chargerId', controller.getPaymentState);
+router.get('/user/:chargerId', controller.getUser); // added by Eamon
+router.get('/getChargerHistory/:chargerId', controller.chargerHistory); // added by Eamon
 router.post('/', checkIfAuthenticated, controller.addCharger);
 router.put('/updateCharger/:id', controller.updateCharger);
 router.put('/setStationOff', controller.setStationOff); // added by Eamon
 router.put('/setStationOn', controller.setStationOn); // added by Eamon
+router.put('/user', controller.setUser); // added by Eamon
+router.delete('/user', controller.removeUser); // added by Eamon
 
 module.exports = router;
